f081990feae40c2240388d35f890702bf5e70e80
lucaa <lucaa@xwiki.com>
2008-06-22 22:59:41 +0000

    XWATCH-161: Make XWiki Watch compatible with IE6 / IE7 * removed useless empty <div /> in the TextComposite, which is rendered with a non-zero size in IE and breaks feed tree layout
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-products/xwiki-watch/trunk@10579 f329d543-caf0-0310-9063-dda96c69346f

--- gwt/src/main/java/com/xpn/xwiki/watch/client/ui/menu/TextWidgetComposite.java
+++ gwt/src/main/java/com/xpn/xwiki/watch/client/ui/menu/TextWidgetComposite.java
@@ -13,25 +13,43 @@ public class TextWidgetComposite extends Composite
 {
     protected Widget mainWidget;
     protected FlowPanel widgetsPanel;
+    protected Panel compositePanel;
+    
     public TextWidgetComposite(Widget mainWidget)
     {
         this.mainWidget = mainWidget;
         this.widgetsPanel = new FlowPanel();
         
-        Panel compositePanel = new FlowPanel();
+        compositePanel = new FlowPanel();
         compositePanel.add(this.mainWidget);
         compositePanel.add(this.widgetsPanel);
+        this.updateCompositePanel();
         //every composite has to call initWidget in their constructors
         initWidget(compositePanel);
     }
     
+    protected void updateCompositePanel() 
+    {
+        if (this.widgetsPanel == null || this.widgetsPanel.getWidgetCount() == 0) {
+            this.compositePanel.remove(this.widgetsPanel);
+        } else if(this.widgetsPanel.getParent() != this.compositePanel) {
+            //add it to the composite panel
+            this.compositePanel.add(this.widgetsPanel);
+        }
+    }
+    
     public void add(TextWidgetComposite cText) {
         this.widgetsPanel.add(cText);
+        //refresh composite panel
+        this.updateCompositePanel();
     }
     
     public void remove(TextWidgetComposite cText) {
         this.widgetsPanel.remove(cText);
+        //refresh composite panel
+        this.updateCompositePanel();
     }
+    
     public List getWidgets() {
         ArrayList widgets = new ArrayList();
         for (int i = 0; i < this.widgetsPanel.getWidgetCount(); i++) {
